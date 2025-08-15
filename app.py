d008bd73f7bb1130d031bdfecad345a5e9078a2c
ArdaWeasley <ardapaydnn@gmail.com>
2025-08-15 13:35:14 +0300

    .

--- app.py
+++ app.py
@@ -178,4 +178,4 @@ with app.app_context():
 
 
 if __name__ == '__main__':
-    app.run(debug=False, port=int(os.environ.get("PORT", 5000)))
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
